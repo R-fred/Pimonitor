09dfbf9c8046b9468f21937d63d636c1c5768ce3
Frederick Chesneau <fchesneau@gmail.com>
2021-10-16 19:03:59 +0200

    edited readme to include examples with agents.

--- pi_monitor/setup.py
+++ pi_monitor/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pi_monitor',
-      version='0.13.7',
+      version='0.14',
       description='A package to monitor your raspberry pi and more!',
       url='',
       author='Frederick Chesneau',
